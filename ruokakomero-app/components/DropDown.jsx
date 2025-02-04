95448a792900f3948e180d2d5345711f9567b53b
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-02-04 15:44:04 +0200

    lisätty säilytyspaikan valinta

---
+++ ruokakomero-app/components/DropDown.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+  import { StyleSheet } from 'react-native';
+  import { Dropdown } from 'react-native-element-dropdown';
+
+  const data = [
+    { label: 'Jääkaappi', value: 'jääkaappi' },
+    { label: 'Pakastin', value: 'pakastin' },
+    { label: 'Ruokakomero', value: 'ruokakomero' },
+  ];
+
+  const DropdownComponent = () => {
+    const [value, setValue] = useState(null);
+
+    return (
+      <Dropdown
+        style={styles.dropdown}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+        search
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder="Valitse säilytyspaikka"
+        searchPlaceholder="Search..."
+        value={value}
+        onChange={item => {
+          setValue(item.value);
+        }}
+      />
+    );
+  };
+
+  export default DropdownComponent;
+
+  const styles = StyleSheet.create({
+    dropdown: {
+      margin: 16,
+      height: 50,
+      borderBottomColor: 'gray',
+      borderBottomWidth: 0.5,
+    },
+    icon: {
+      marginRight: 5,
+    },
+    placeholderStyle: {
+      fontSize: 16,
+    },
+    selectedTextStyle: {
+      fontSize: 16,
+    },
+    iconStyle: {
+      width: 20,
+      height: 20,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 16,
+    },
+  });
