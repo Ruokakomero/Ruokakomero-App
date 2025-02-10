441d1874162015b50fdb408bc4b51e1ad192f809
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-02-10 18:17:02 +0200

    lisätty navigaatioon ikonit

--- ruokakomero-app/App.js
+++ ruokakomero-app/App.js
@@ -3,16 +3,36 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from './components/HomePage';
 import AddProductsPage from './components/AddProductsPage';
 import ListProducts from './components/ListProducts';
+import { Ionicons } from "@expo/vector-icons";
 
-const Tab = createBottomTabNavigator();
-  
-export default function App() {
-  return (
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
     <NavigationContainer>
-        <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Etusivu") {
+              iconName = "home";
+            } else if (route.name === "Ainesosat") {
+              iconName = "list";
+            } else if (route.name === "Lisää ainesosia") {
+              iconName = "add-circle";
+            } else if (route.name === "Reseptit") {
+              iconName = "book";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tab.Screen name="Etusivu" component={HomePage} />
         <Tab.Screen name="Ainesosat" component={ListProducts} />
         <Tab.Screen name="Lisää ainesosia" component={AddProductsPage} />
+        <Tab.Screen name="Reseptit" component={AddProductsPage} />
       </Tab.Navigator>
     </NavigationContainer>
   );
