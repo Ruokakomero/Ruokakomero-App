6e6093a1311d10c43775b30beab70f82a459331c
jkaitasalo <jouni.kaitasalo@hotmail.com>
2025-02-21 12:10:00 +0200

    Firebase-palvelimen käyttöönotto
    - Lisätty Firebase SDK ja konfiguroitu se projektiin

---
+++ ruokakomero-app/constants/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDH2IaYLc-P0md-cmZvovz7jtXfsVrHw08",
+  authDomain: "ruokakomero-3022b.firebaseapp.com",
+  databaseURL: "https://ruokakomero-3022b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ruokakomero-3022b",
+  storageBucket: "ruokakomero-3022b.firebasestorage.app",
+  messagingSenderId: "934777900518",
+  appId: "1:934777900518:web:aa05684a40149706701a27",
+  measurementId: "G-79P6W4NWGZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
